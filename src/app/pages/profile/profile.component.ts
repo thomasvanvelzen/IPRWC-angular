3e3c2df873ba6a48dfde36ec62b3521c3b2e5795
Thomas van Velzen <50317402+thomasvanvelzen@users.noreply.github.com>
2023-03-22 11:10:19 +0100

    Added user feedback for profile updating

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,12 +3,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProfileService } from './profile.service';
 import { UserProfile } from 'src/app/typings/user-profile.type';
 import { TokenService } from 'src/app/services/token.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
+  public error: string | null = null;
+  public success: boolean = false;
+
   constructor(private service: ProfileService, private token: TokenService) {
     this.getProfile();
   }
@@ -70,10 +74,15 @@ export class ProfileComponent {
   public updateProfile() {
     const authority = this.token.getTokenFromLocalStorage();
     this.profile.id = authority.user.id;
-    this.service.updateProfile(this.profile).subscribe((profile) => {
-      if (!profile) return;
-      this.profile = profile;
-    });
+    firstValueFrom(this.service.updateProfile(this.profile))
+      .then((profile) => {
+        if (!profile) return;
+        this.profile = profile;
+        this.handleSuccess(3);
+      })
+      .catch((err) => {
+        this.error = err.error.message;
+      });
   }
 
   public getProfile() {
@@ -86,5 +95,12 @@ export class ProfileComponent {
   public logout() {
     this.token.destroyTokenFromLocalStorage();
     window.location.reload();
+  }
+
+  handleSuccess(seconds: number) {
+    this.success = true;
+    setTimeout(() => {
+      this.success = false;
+    }, seconds * 1000);
   }
 }
