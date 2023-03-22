e9d99b83b1ef805faac205224d422083e0732276
Thomas van Velzen <50317402+thomasvanvelzen@users.noreply.github.com>
2023-03-22 10:50:38 +0100

    Error handling product creation

--- src/app/pages/admin/product-creation/product-creation.service.ts
+++ src/app/pages/admin/product-creation/product-creation.service.ts
@@ -21,6 +21,9 @@ export class ProductCreationService {
   public error: string | null = null;
 
   public postProduct(product: Product) {
+    this.handleError(product);
+    if (this.error) return;
+
     firstValueFrom(
       this.http.post<Product[]>(
         `${environment.apiURL}/product`,
@@ -31,7 +34,6 @@ export class ProductCreationService {
       .then((products) => {
         this.global.products = products;
         this.handleSuccess(3);
-    
       })
       .catch((err) => {
         if (err.error.status === 409)
@@ -44,5 +46,27 @@ export class ProductCreationService {
     setTimeout(() => {
       this.success = false;
     }, seconds * 1000);
+  }
+
+  handleError(product: Product) {
+    if (!product) {
+      this.error = 'Product is undefined';
+    } else if (!product.name) {
+      this.error = 'Product name is undefined';
+    } else if (product.name.length < 3) {
+      this.error = 'Product name must be at least 3 characters long';
+    } else if (!product.price) {
+      this.error = 'Product price is undefined';
+    } else if (product.price < 0) {
+      this.error = 'Product price must be greater than 0';
+    } else if (!product.description) {
+      this.error = 'Product description is undefined';
+    } else if (product.description.length < 10) {
+      this.error = 'Product description must be at least 10 characters long';
+    } else if (!product.image) {
+      this.error = 'Product image is undefined';
+    } else {
+      this.error = null;
+    }
   }
 }
